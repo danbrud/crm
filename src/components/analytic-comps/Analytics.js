6590a6a7b6fd0217effdb1698c2cd85f55ca63f9
Danny <dannybrudner@gmail.com>
2019-05-12 04:23:53 -0400

    Added main components and routes. Completed navbar.

---
+++ src/components/analytic-comps/Analytics.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Analytics extends Component {
+
+    render() {
+        return(
+            <div>
+                All analytics here.
+            </div>
+        )
+    }
+}
+
+export default Analytics
