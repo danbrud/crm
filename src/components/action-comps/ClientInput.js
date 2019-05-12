dacc5c06e79ae2120236047ec2a8b21f416cc1ef
Danny <dannybrudner@gmail.com>
2019-05-12 10:20:04 -0400

    Completed Client Input component

--- src/components/action-comps/ClientInput.js
+++ src/components/action-comps/ClientInput.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react'
 
 class ClientInput extends Component {
 
+    constructor(){
+        super()
+        this.state = {
+            selectedName: ""
+        }
+    }
+
+    updateName = e => this.setState({selectedName: e.target.value}, function() { this.updateClientID() })
+
+    createDataListOptions = () => this.props.clients.map(c => <option key={c._id} value={c.name} />)
+
+    updateClientID = () => {
+        let client = this.props.clients.find(c => c.name === this.state.selectedName)
+        if(client) { this.props.updateClientID(client._id) }
+    }
+
     render() {
-        return(
+        return (
             <div>
                 <p>Client: </p>
-                <input type="text" placeholder="Client Name" list="clients"/>
+                <input type="text" placeholder="Client Name" list="clients" value={this.state.selectedName} onInput={this.updateName}/>
+                <datalist id="clients">
+                    {this.createDataListOptions()}
+                </datalist>
             </div>
         )
     }
--- src/components/action-comps/UpdateClient.js
+++ src/components/action-comps/UpdateClient.js
@@ -8,9 +8,12 @@ class UpdateClient extends Component {
     constructor() {
         super()
         this.state = {
-            clients: []
+            clients: [],
+            clientIdToUpdate: ""
         }
     }
+
+    updateClientID = clientIdToUpdate => this.setState({ clientIdToUpdate })
 
     getClients = async () => {
         let clients = await axios.get('http://localhost:3001/clients/actions')
@@ -22,26 +25,39 @@ class UpdateClient extends Component {
         this.setState({ clients })
     }
 
+    createEmailDropdownElement = () => {
+        return (
+            <select name="emailType">
+                <option value="a">A</option>
+                <option value="b">B</option>
+                <option value="c">C</option>
+                <option value="d">D</option>
+            </select>
+        )
+    }
+
+    createOwnerDropdownElement = () => {
+
+    }
+
     render() {
         return (
             <div id="update-action">
                 <h3>UPDATE</h3>
-                <ClientInput clients={this.state.clients} />
+                <ClientInput updateClientID={this.updateClientID} clients={this.state.clients} />
+
                 <div className="update-client" id="transfer-ownership">
                     <p>Transfer ownership to:</p>
                     <select name="" id=""></select>
                     <div id="transfer-btn">TRANSFER</div>
                 </div>
+
                 <div className="update-client" id="send-email">
                     <p>Send email:</p>
-                    <select name="emailType">
-                        <option value="a">A</option>
-                        <option value="b">B</option>
-                        <option value="c">C</option>
-                        <option value="d">D</option>
-                    </select>
+                    {this.createEmailDropdownElement()}
                     <div id="send-email-btn">SEND</div>
                 </div>
+                
                 <div className="update-client" id="declare-sale">
                     <p>Declare sale!</p>
                     <div></div>
