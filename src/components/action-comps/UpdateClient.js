39dc573a6803260652edf33fb994ca0cffe91b83
Danny <dannybrudner@gmail.com>
2019-05-12 09:35:21 -0400

    Created layout for Update Client component

--- src/components/action-comps/UpdateClient.js
+++ src/components/action-comps/UpdateClient.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
+import ClientInput from './ClientInput';
+import axios from 'axios';
+import '../styles/UpdateClient.css'
 
 class UpdateClient extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+            clients: []
+        }
+    }
+
+    getClients = async () => {
+        let clients = await axios.get('http://localhost:3001/clients/actions')
+        return clients.data
+    }
+
+    componentDidMount = async () => {
+        let clients = await this.getClients()
+        this.setState({ clients })
+    }
+
     render() {
-        return(
-            <div>
-                <h2>UPDATE</h2>
-            </div>
+        return (
+            <div id="update-action">
+                <h3>UPDATE</h3>
+                <ClientInput clients={this.state.clients} />
+                <div className="update-client" id="transfer-ownership">
+                    <p>Transfer ownership to:</p>
+                    <select name="" id=""></select>
+                    <div id="transfer-btn">TRANSFER</div>
+                </div>
+                <div className="update-client" id="send-email">
+                    <p>Send email:</p>
+                    <select name="emailType">
+                        <option value="a">A</option>
+                        <option value="b">B</option>
+                        <option value="c">C</option>
+                        <option value="d">D</option>
+                    </select>
+                    <div id="send-email-btn">SEND</div>
+                </div>
+                <div className="update-client" id="declare-sale">
+                    <p>Declare sale!</p>
+                    <div></div>
+                    <div id="declare-sale-btn">DECLARE</div>
+                </div >
+            </div >
         )
     }
 }
