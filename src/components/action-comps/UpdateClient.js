58f86a703698be316d6386af7a00c8e8f39765d5
Danny <dannybrudner@gmail.com>
2019-05-12 11:16:34 -0400

    Completed Update Client component including put requests

--- src/components/action-comps/UpdateClient.js
+++ src/components/action-comps/UpdateClient.js
@@ -9,7 +9,10 @@ class UpdateClient extends Component {
         super()
         this.state = {
             clients: [],
-            clientIdToUpdate: ""
+            clientIdToUpdate: "",
+            newOwner: "",
+            newEmailType: "",
+            disableEmptySelect: { newOwner: false, newEmailType: false }
         }
     }
 
@@ -27,11 +30,12 @@ class UpdateClient extends Component {
 
     createEmailDropdownElement = () => {
         return (
-            <select name="emailType">
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-                <option value="d">D</option>
+            <select name="newEmailType" value={this.state.newEmailType} onChange={this.handleSelections}>
+                <option selected disabled={this.state.disableEmptySelect.newEmailType ? true : null}> -- select an email -- </option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
             </select>
         )
     }
@@ -39,10 +43,31 @@ class UpdateClient extends Component {
     createOwnerDropdownElement = () => {
         let availableOwners = []
         this.state.clients.forEach(c => availableOwners.some(ao => ao === c.owner) ?
-                                                    null : availableOwners.push(c.owner))
+            null : availableOwners.push(c.owner))
 
         return availableOwners.map(ao => <option value={ao}>{ao}</option>)
     }
+
+    updateUser = async (propertyToUpdate, updateValue) => {
+        await axios.put(`client/${this.state.clientIdToUpdate}/${propertyToUpdate}`, updateValue)
+    }
+
+    handleSelections = e => {
+        if (this.state.disableEmptySelect[e.target.name]) {
+            this.setState({ [e.target.name]: e.target.value })
+        } else {
+            let disableEmptySelect = { ...this.state.disableEmptySelect }
+            disableEmptySelect[e.target.name] = true
+
+            this.setState({ [e.target.name]: e.target.value, disableEmptySelect })
+        }
+    }
+
+    transferOwnership = () => this.updateUser('owner', this.state.newOwner)
+
+    sendEmail = () => this.updateUser('emailType', this.state.newEmailType)
+
+    declareSale = () => this.updateUser('sold', true)
 
     render() {
         return (
@@ -52,7 +77,8 @@ class UpdateClient extends Component {
 
                 <div className="update-client" id="transfer-ownership">
                     <p>Transfer ownership to:</p>
-                    <select>
+                    <select name="newOwner" value={this.state.newOwner} onChange={this.handleSelections}>
+                        <option selected disabled={this.state.disableEmptySelect.newOwner ? true : null}> -- select an owner -- </option>
                         {this.createOwnerDropdownElement()}
                     </select>
                     <div id="transfer-btn">TRANSFER</div>
