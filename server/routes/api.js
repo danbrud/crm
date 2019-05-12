c7f74724e02840ae9bda8269def6f36ba2139871
Danny <dannybrudner@gmail.com>
2019-05-12 04:55:03 -0400

    Added post clients routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// const Transaction = require('../model/Transaction')
+const Client = require('../model/Client')
 
 
 router.get('/sanity', function (req, res) {
@@ -8,6 +8,27 @@ router.get('/sanity', function (req, res) {
 })
 
 
+
+
+router.post('/clients', function (req, res) {
+    let reqClient = req.body
+
+    let newClient = new Client({
+        _id: reqClient._id,
+        name: reqClient.name,
+        email: reqClient.email,
+        firstContact: reqClient.firstContact,
+        emailType: reqClient.emailType,
+        sold: reqClient.sold,
+        owner: reqClient.owner,
+        country: reqClient.country
+    })
+
+    let save = newClient.save()
+    save.then(function(client) {
+        res.send(`Saved client - id: ${client._id}, name: ${client.name}`)
+    })
+})
 
 
 module.exports = router
