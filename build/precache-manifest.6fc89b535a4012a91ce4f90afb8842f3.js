07ee88c019787dad3f3488acf8e1cd37f0d201f9
Danny <dannybrudner@gmail.com>
2019-05-19 15:31:20 +0300

    ignored src for heroku

---
+++ build/precache-manifest.6fc89b535a4012a91ce4f90afb8842f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47542e1d19d2c22264c1d578e87ca7ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96d4582a1bde58e46142",
+    "url": "/static/css/main.d3e04743.chunk.css"
+  },
+  {
+    "revision": "c53738e647313f89df88",
+    "url": "/static/js/2.d681bfbc.chunk.js"
+  },
+  {
+    "revision": "96d4582a1bde58e46142",
+    "url": "/static/js/main.6941f37f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
