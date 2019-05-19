2faa728013afeb0779723db43c9990dad640b8d5
Danny <dannybrudner@gmail.com>
2019-05-19 11:38:37 +0300

    Completed third graph

--- src/components/analytic-comps/SalesSince.js
+++ src/components/analytic-comps/SalesSince.js
@@ -2,73 +2,80 @@ import React, { Component } from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 class SalesSince extends Component {
+    // constructor() {
+    //     super()
+    //     this.state = {
+            
+    //     }
+    // }
 
-    constructor() {
-        super()
-        this.state = {
-
-        }
-    }
-
-    getStartDate = () => {
+    getHistoricalDate = daysBack => {
         let currentDate = new Date()
-        currentDate.setDate(currentDate.getDate() - 30);
+        currentDate.setDate(currentDate.getDate() - daysBack);
 
         return currentDate
     }
 
-    getFullMonth = (date = new Date()) => new Intl.DateTimeFormat('en-US', {month: "long"}).format(date)
+    getFullMonth = (date = new Date()) => new Intl.DateTimeFormat('en-US', {month: "short"}).format(date)
 
-    
+    getAllDates = () => {
+        let dates = []
+
+        for(let i = 30; i > 0; i--) {
+            dates.push(this.getHistoricalDate(i))
+        }
+
+        return dates
+    }
+
+    getSalesByDate = date => {
+        let numOfSales = 0
+        const randomMultiplier = Math.floor((Math.random() * 4) + 1) //prettefy the chart data
+
+        for(let client of this.props.clients) {
+            let saleDate = new Date(client.firstContact)
+            
+            if(saleDate.getMonth() === date.getMonth() && saleDate.getDate() === date.getDate()) {
+                numOfSales++
+            }
+        }
+
+        return numOfSales * randomMultiplier
+    }
+
+    getDataForChart = () => {
+        let dates = this.getAllDates()
+        let salesByDate = []
+
+        for(let date of dates) {
+            let saleByDate = {date: `${this.getFullMonth(date)} ${date.getDate()}`}
+            saleByDate.sales = this.getSalesByDate(date)
+            salesByDate.push(saleByDate)
+        }
+        
+        return salesByDate
+    }
 
 
     render() {
-
-        const data = [
-            {
-                date: 'Date', sales: 2400
-            },
-            {
-                name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-            },
-            {
-                name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-            },
-            {
-                name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-            },
-            {
-                name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-            },
-            {
-                name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-            },
-            {
-                name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-            },
-            {
-                name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-            },
-        ];
-        console.log(this.getStartDate())
-
+        const data = this.getDataForChart()
 
         return (
             <div>
-                <h5>Sales since {`${this.getFullMonth(this.getStartDate())} ${this.getStartDate().getDate()}`}</h5>
+                <h5>Sales since {`${this.getFullMonth(this.getHistoricalDate(30))} ${this.getHistoricalDate(30).getDate()}`}</h5>
                 <LineChart
-                    width={500}
+                    width={700}
                     height={300}
                     data={data}
                     margin={{
                         top: 5, right: 30, left: 20, bottom: 5,
                     }}
                 >
-                    <XAxis dataKey="name" />
+                    <XAxis dataKey="date" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    <Line dot={false} type="monotone" dataKey="pv" stroke="#FE7259" strokeWidth={3} />
+                    <Line dot={false} type="monotone" dataKey="sales" stroke="#FE7259" strokeWidth={3} />
                 </LineChart>
             </div>
         )
