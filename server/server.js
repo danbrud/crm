a935364720a04113e48c4b5a80c3f0679b2414df
danbrud <dannybrudner@users.noreply.github.com>
2020-12-30 16:12:31 +0200

    add title and change server location

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const api = require('./server/routes/api')
+const api = require('./routes/api')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
