b9b34b86dcf3c71eb781443281349f7a89993b9c
danbrud <dannybrudner@users.noreply.github.com>
2020-12-31 15:30:08 +0200

    fixed path for catch all route

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use('/api', api)
 
 if (!inDevMode) {
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
     })
 }
 
